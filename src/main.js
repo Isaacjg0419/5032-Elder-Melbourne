420477a54b83e452fa7d0ee6a2ea90c2393852c7
iscfen <1412591585@qq.com>
2024-08-12 21:35:47 +1000

    make the navbar and home page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/base.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="introduction-section">
+      <div class="row">
+        <div class="col-6">
+          <h1>Elder Community Introduction</h1>
+          <p style="padding-left:40px;margin-top: 20px;">The City of Melbourne offers a range of programs and<br> services to support older people to remain active,
+            independent and living at home.​</p>
+        </div>
+        <div class="col-6">
+          <img :src="oldPeople" alt="old-people here" class="responsive-image">
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import oldPeople from '@/assets/older-people.jpg';
+import Navbar from '../components/NavBar.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.introduction-section {
+  margin-top: 20px;
+  padding: 20px;
+}
+
+.responsive-image {
+  margin-left: 10px;
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+</style>
