ddeb08ad2a3817e8605648b68e9828eb628360d8
iscfen <1412591585@qq.com>
2024-08-22 22:39:31 +0800

    init the firebase sdk

---
+++ src/data/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaEyQ5hYpaWNvWFWMPo-7vYYt3_Fb9iRE",
+  authDomain: "db-67c2b.firebaseapp.com",
+  projectId: "db-67c2b",
+  storageBucket: "db-67c2b.appspot.com",
+  messagingSenderId: "342994586670",
+  appId: "1:342994586670:web:fd463ca18280aea404a3bf",
+  measurementId: "G-G627GEDMKJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
