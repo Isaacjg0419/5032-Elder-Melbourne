1ae6723b3be5ddb188379f1dad5702b619dc2d38
iscfen <1412591585@qq.com>
2024-09-24 19:07:25 +1000

    init the hosting with git

--- functions/index.js
+++ functions/index.js
@@ -1,52 +1,19 @@
-const { onRequest } = require('firebase-functions/v2/https')
-const admin = require('firebase-admin')
-const cors = require('cors')
-const sgMail = require('@sendgrid/mail')
-const functions = require('firebase-functions')
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// Initialize Firebase Admin SDK
-admin.initializeApp()
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-sgMail.setApiKey(functions.config().sendgrid.apikey)
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-// Create CORS middleware with options
-const corsOptions = {
-  origin: true // Allows all origins
-}
-
-const corsMiddleware = cors(corsOptions)
-
-// Function to send an email
-exports.sendEmail = onRequest((req, res) => {
-  corsMiddleware(req, res, async () => {
-    try {
-      const { to, subject, text } = req.body
-
-      if (!to || !subject || !text) {
-        return res.status(400).send('Missing required fields')
-      }
-
-      const msg = {
-        to,
-        from: 'jfen0016@student.monash.edu',
-        subject,
-        text,
-        html: `<strong>${text}</strong>`
-        // attachments: [
-        //   {
-        //     content: attachmentContent,
-        //     filename: attachmentFileName,
-        //     type: 'application/pdf',
-        //     disposition: 'attachment'
-        //   }
-        // ]
-      }
-
-      await sgMail.send(msg)
-      res.status(200).send('Email sent successfully')
-    } catch (error) {
-      console.error('Error sending email:', error.message)
-      res.status(500).send('Error sending email')
-    }
-  })
-})
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
